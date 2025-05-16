d773e3da9541d60893f5309003f0ddb3c614c44f
Juana Zabaleta <74736103+juaana@users.noreply.github.com>
2025-05-16 20:59:36 -0300

    enprolijado de rutas

--- config/routes.php
+++ config/routes.php
@@ -15,28 +15,43 @@ use App\Controllers\{
 use App\Middleware\RequireAPIKey;
 use Dotenv\Dotenv;
 
+// Cargar variables de entorno
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
+// RUTAS PÚBLICAS 
 $app->group('', function (RouteCollectorProxy $group) {
     $group->post('/registro', SignupController::class . ':create'); 
     $group->post('/login', LoginController::class . ':create');
     $group->get('/logout', LoginController::class . ':destroy');
-
-    $group->put('/usuarios/{usuario:[0-9]+}', ProfileController::class . ':update')->add(RequireAPIKey::class);
-    $group->get('/usuarios/{usuario:[0-9]+}', ProfileController::class . ':showUserData')->add(RequireAPIKey::class);
-
-    $group->post('/partidas', PartidaController::class . ':start')->add(RequireAPIKey::class);
-    $group->post('/jugadas', JugadaController::class . ':registrarJugada')->add(RequireAPIKey::class);
-
     $group->get('/cartas', CardsController::class . ':showByData');
-
-    $group->get('/usuarios/{usuario:[0-9]+}/mazos', MazoController::class . ':getUserMazos')->add(RequireAPIKey::class); 
-    $group->delete('/mazos/{id:[0-9]+}', MazoController::class . ':delete')->add(RequireAPIKey::class); 
-    $group->put('/mazos/{id:[0-9]+}', MazoController::class . ':update')->add(RequireAPIKey::class);  
-    $group->post('/mazos', MazoController::class . ':create')->add(RequireAPIKey::class); 
-
-    $group->get('/usuarios/{usuario:[0-9]+}/partidas/{partida:[0-9]+}/cartas', PartidaController::class . ':cartasEnMano')->add(RequireAPIKey::class);
+    $group->get('/estadisticas', EstadisticasController::class . ':show');
 });
 
-$app->get('/estadisticas', EstadisticasController::class . ':show');
+
+
+// RUTAS PRIVADAS 
+$app->group('', function (RouteCollectorProxy $group) {
+
+    //  Perfil de usuario
+    $group->group('/usuarios', function (RouteCollectorProxy $group) {
+        $group->put('/{usuario:[0-9]+}', ProfileController::class . ':update');
+        $group->get('/{usuario:[0-9]+}', ProfileController::class . ':showUserData');
+        $group->get('/{usuario:[0-9]+}/mazos', MazoController::class . ':getUserMazos');
+        $group->get('/{usuario:[0-9]+}/partidas/{partida:[0-9]+}/cartas', PartidaController::class . ':cartasEnMano');
+    });
+
+    //  Partidas
+    $group->post('/partidas', PartidaController::class . ':start');
+
+    // Jugadas
+    $group->post('/jugadas', JugadaController::class . ':registrarJugada');
+
+    // Mazos
+    $group->group('/mazos', function (RouteCollectorProxy $group){
+        $group->delete('/{id:[0-9]+}', MazoController::class . ':delete'); 
+        $group->put('/{id:[0-9]+}', MazoController::class . ':update');  
+        $group->post('', MazoController::class . ':create'); 
+    });
+
+})->add(RequireAPIKey::class);
